72621e283e4ac0365860a866b25301b0a9e54615
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-07-17 12:12:14 +0600

    Tweak Mongoose model for tests

--- tests/models/Test.js
+++ tests/models/Test.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const algoliaPlugin = require('../../src/mongoose-algolia-plugin.js');
+const algoliaPlugin = require('../../index.js');
 
 let TestSchema = new mongoose.Schema({
   name: String
 });
 
-TestSchema.plugin(algoliaPlugin, { index: true, schemaName: 'Test' });
+TestSchema.plugin(algoliaPlugin, {
+  index: true,
+  schemaName: 'Test',
+  applicationId: 'myId',
+  apiKey: 'myKey'
+});
 
 mongoose.model('Test', TestSchema);
